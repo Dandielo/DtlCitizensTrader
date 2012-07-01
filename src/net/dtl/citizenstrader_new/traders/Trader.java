8e8200e7435b596b101778e65653aa6e287f8642
dandielo <daniel.penkala@gmail.com>
2012-07-01 02:00:53 +0200

    Inventory name change (sounds better)

--- src/net/dtl/citizenstrader_new/traders/Trader.java
+++ src/net/dtl/citizenstrader_new/traders/Trader.java
@@ -93,7 +93,7 @@ public abstract class Trader {
 		 * 
 		 */
 		traderStock = tradderNpc.getTrait(InventoryTrait.class);
-		inventory = traderStock.inventoryView(54, tradderNpc.getName() + " selling");
+		inventory = traderStock.inventoryView(54, tradderNpc.getName() + " trader");
 		traderStatus = TraderStatus.PLAYER_SELL;
 		
 		/* *
