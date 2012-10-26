ffbd62a7ff53580c5c5f882b0ccac2e5a72535ae
dandielo <daniel.penkala@gmail.com>
2012-10-26 23:03:26 +0200

    Minor change (future market trader)

--- src/net/dtl/citizens/trader/objects/MarketItem.java
+++ src/net/dtl/citizens/trader/objects/MarketItem.java
@@ -1,12 +1,15 @@
 package net.dtl.citizens.trader.objects;
 
+import java.util.Date;
+
 public class MarketItem extends StockItem {
 	protected String itemOwner;
+	protected Date time;
 	
 	public MarketItem(String data) {
 		super(data);
 	}
-
+	
 	public String getItemOwner()
 	{
 		return itemOwner;
