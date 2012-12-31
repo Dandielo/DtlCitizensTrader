a4692605b2da7f5799b2f36096e5d569e14cc79e
dandielo <daniel.penkala@gmail.com>
2012-12-31 19:34:53 +0100

    update for jenkins

---
+++ src/main/java/net/dtl/citizens/trader/denizen/commands/PatternCommand.java
@@ -0,0 +1,28 @@
+package net.dtl.citizens.trader.denizen.commands;
+
+import net.aufdemrand.denizen.exceptions.CommandExecutionException;
+import net.aufdemrand.denizen.exceptions.InvalidArgumentsException;
+import net.aufdemrand.denizen.scripts.ScriptEntry;
+import net.dtl.citizens.trader.denizen.AbstractDenizenCommand;
+
+public class PatternCommand extends AbstractDenizenCommand {
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void execute(String arg0) throws CommandExecutionException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void parseArgs(ScriptEntry arg0) throws InvalidArgumentsException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
